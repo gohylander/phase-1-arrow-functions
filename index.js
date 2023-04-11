ad9de6fc5e908e0b537afee9578f44418fc1ab91
George Hyland <gohylander@gmail.com>
2023-04-11 10:04:12 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    let a = 2000;
+    let b = 100;
+    return 20;
+    }
+    
+    const square = (x) => x ** 2;
+    
+    const add = (a,b) => a + b;
+    let a = 3;
+    let b = 4;
+    
